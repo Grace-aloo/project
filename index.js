f7a6e75b7364d282ef5c2463c75207a24d1b8c35
Grace-aloo <graceaaloo@gmail.com>
2022-12-16 09:40:12 +0300

    linked js to html

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded",() => {
     const login = document.getElementById('login');
     const form = document.getElementById('formDiv');
     const home = document.getElementById('home');
+    const header = document.getElementById('header');
+    const footer = document.getElementById('footer');
     login.addEventListener('click',() => {
+        home.style.display = "none";
+        header.style.display = "none";
+        footer.style.display = "none";
+        form.removeAttribute('hidden');
 
     })
 })
