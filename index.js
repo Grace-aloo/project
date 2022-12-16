d97f6dab535985d1e17256e167e35dc1f0f6b930
Grace-aloo <graceaaloo@gmail.com>
2022-12-16 09:11:17 +0300

    login page done header and footer too

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",() => {
+    const login = document.getElementById('login');
+    const form = document.getElementById('formDiv');
+    const home = document.getElementById('home');
+    login.addEventListener('click',() => {
+
+    })
+})
